b4d703b30ae002fe0b0447486dfab143288a4316
yago rodrigues <yagodev3@gmail.com>
2022-09-13 20:46:53 -0300

    Modificação código Java

--- java/Contagem.java
+++ java/Contagem.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 
 public class Contagem {
     
+
+  public static void main(String[] args) {
     Scanner prompt = new Scanner(System.in);
     int n, nota, cont = 0, i = 0;
     
-    public void contagemdenumeros(){
+    
       System.out.println("Quantidade de alunos: ");
-      int n = prompt.nextInt();
+      n = prompt.nextInt();
 
       while(i < n){
         i = i + 1;
-        System.out.println("Nota do aluno: " + i);
+        System.out.println("Digite a " + i + "ª nota: ");
         nota = prompt.nextInt();
         if(nota < 7){
           cont = cont + 0;
@@ -22,5 +24,7 @@ public class Contagem {
 
       System.out.println("Existem " + n + " alunos matriculados");
       System.out.println("Foram aprovados " + cont + " aprovados");
-    }
+    
+  }
+    
 }
